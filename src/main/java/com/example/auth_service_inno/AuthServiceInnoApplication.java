d10180174b5a8e078dd9563f382047437ddc227c
Aglaya Olinevich <aglaya.olinevich@imbuesystems.com>
2026-05-31 19:45:31 +0300

    first commit

---
+++ src/main/java/com/example/auth_service_inno/AuthServiceInnoApplication.java
@@ -0,0 +1,13 @@
+package com.example.auth_service_inno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceInnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceInnoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/auth_service_inno/AuthServiceInnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auth_service_inno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceInnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
